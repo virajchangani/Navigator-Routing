343315c10be4fa6d4b6a0ecb8806fd14b13c9f31
Viraj Changani <145748780+virajchangani@users.noreply.github.com>
2024-09-16 11:15:00 +0530

    Update Home.jsx

--- navigator/src/Componets/Home.jsx
+++ navigator/src/Componets/Home.jsx
@@ -5,35 +5,38 @@ function Home({ record }) {
   return (
     <div>
       <h1>Library Management System</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Enrollment Num</th>
-            <th>Student Name</th>
-            <th>Student Field</th>
-            <th>Book Name</th>
-            <th>Book Author Name</th>
-            <th>Purchase Date</th>
-            <th>Return Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          {record.map((e) => (
-            <tr key={e.i}>
-              <td>{e.id}</td>
-              <td>{e.enrollmentNumber}</td>
-              <td>{e.studentName}</td>
-              <td>{e.studentField}</td>
-              <td>{e.bookName}</td>
-              <td>{e.bookAuthor}</td>
-              <td>{e.purchaseDate}</td>
-              <td>{e.returnDate}</td>
+      {record.length > 0 ? (
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Enrollment Num</th>
+              <th>Student Name</th>
+              <th>Student Field</th>
+              <th>Book Name</th>
+              <th>Book Author Name</th>
+              <th>Purchase Date</th>
+              <th>Return Date</th>
             </tr>
-            
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {record.map((e) => (
+              <tr key={e.id}>
+                <td>{e.id}</td>
+                <td>{e.enrollmentNumber}</td>
+                <td>{e.studentName}</td>
+                <td>{e.studentField}</td>
+                <td>{e.bookName}</td>
+                <td>{e.bookAuthor}</td>
+                <td>{e.purchaseDate}</td>
+                <td>{e.returnDate}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p style={{textAlign:'center', color:'red'}}>No data available</p>
+      )}
     </div>
   );
 }
